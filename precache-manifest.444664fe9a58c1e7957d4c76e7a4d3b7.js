fa2d82f6ce23280eaacf5b6595cf48d50ceb4d72
Maicon Avila <maiconavila00@gmail.com>
2020-06-10 20:32:43 -0300

    Updates

---
+++ precache-manifest.444664fe9a58c1e7957d4c76e7a4d3b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfd38faeafce459168587178346f683",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "eb25c0a728bba7111898",
+    "url": "/pomodoro/static/js/2.43855e6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pomodoro/static/js/2.43855e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb16fea5d2b2fbacf43",
+    "url": "/pomodoro/static/js/main.10e9b5bc.chunk.js"
+  },
+  {
+    "revision": "be88404afcd08bafaf7e",
+    "url": "/pomodoro/static/js/runtime-main.a40df6f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bd7f7a231641a6fc3770b7f046e77db.js"
+  "/pomodoro/precache-manifest.444664fe9a58c1e7957d4c76e7a4d3b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
